02bd833f9c7dde426c181d85ecec55b432a9d8de
Ayo Adesugba <ayoadesugba@gmail.com>
2022-11-12 19:48:11 -0500

    Update AuthRequestBuilder.swift

--- ios/Classes/AuthRequestBuilder.swift
+++ ios/Classes/AuthRequestBuilder.swift
@@ -15,9 +15,9 @@ class AuthRequestBuilder: AuthRequestBuilderProtocol {
     }
     
     func requestFor(socketID: String, channelName: String) -> URLRequest? {
-        print("Socket ID: \(socketID)")
-        print("channelName: \(channelName)")
-        print("URL: \(pusherAuth.endpoint)")
+        Utils.debugLog(msg: "Socket ID: \(socketID)")
+        Utils.debugLog(msg: "channelName: \(channelName)")
+        Utils.debugLog(msg: "URL: \(pusherAuth.endpoint)")
         
         var request = URLRequest(url: URL(string: pusherAuth.endpoint)!)
         request.httpMethod = "POST"
